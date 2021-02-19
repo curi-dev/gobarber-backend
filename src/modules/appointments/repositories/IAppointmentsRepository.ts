17e4bd06e2bbfa86af0146d0797fcc20e6fe452a
Tiago Lindgren Curi <tcuri@unicad.local>
2021-02-19 19:21:55 -0300

    atualizando projeto

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,8 @@
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import ICreateAppointmentDTO from '../dtos/ICreateAppoitmenteDTO';
 
 export default interface IAppointmensRepository {
+    create(data: ICreateAppointmentDTO): Promise<Appointment>;
     findByDate(date: Date): Promise<Appointment | undefined>;
 }
 
